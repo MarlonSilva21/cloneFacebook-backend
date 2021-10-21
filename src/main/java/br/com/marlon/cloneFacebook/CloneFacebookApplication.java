5e73538bf2f7f60fc26f8cf23ec4a01d5f5cdd3a
MarlonSilva21 <josemarlondasilva48@gmail.com>
2021-10-21 18:35:50 -0300

    adicionando swagger

--- src/main/java/br/com/marlon/cloneFacebook/CloneFacebookApplication.java
+++ src/main/java/br/com/marlon/cloneFacebook/CloneFacebookApplication.java
@@ -2,9 +2,16 @@ package br.com.marlon.cloneFacebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class CloneFacebookApplication {
+
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloneFacebookApplication.class, args);
